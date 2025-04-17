fca69d5e8e5aa0d660459e5decc8b445331ca679
Jeong Eun Lee <naturetoscience2013@gmail.com>
2025-04-18 01:10:19 +0900

    clean working version

---
+++ README.md
@@ -0,0 +1,2 @@
+# CNUMED 상조회
+정상 작동하는 Vite + React 앱
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>CNUMED 상조회</h1>
+      <p>정상 작동하는 Vite + React 앱입니다.</p>
+    </div>
+  );
+}
